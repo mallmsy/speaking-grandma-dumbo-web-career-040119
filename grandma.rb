6067fb118d814520a4edeaf18ef66413392e15a8
Mallory Woods <malvwoods@gmail.com>
2019-03-05 17:25:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
