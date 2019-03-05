7665a77c81227b218cdd1ac45ac9af3fea740c36
Mallory Woods <malvwoods@gmail.com>
2019-03-05 17:19:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++
@@ -1,7 +0,0 @@
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase
-    puts "NO, NOT SINCE 1938!"
-  else puts "HUH?! SPEAK UP, SONNY!"
-end
